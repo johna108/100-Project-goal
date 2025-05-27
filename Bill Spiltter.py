81848029a1057d4032fa5025490f60bc8bb2a4b0
johna108 <jhonsamliv@gmail.com>
2025-05-27 21:28:33 +0530

    g

---
+++ Bill Spiltter.py
@@ -0,0 +1,5 @@
+print("Welcome to the bill splitter")
+a = int(input("What was the final bill amount: "))
+b = int(input("Total number of people?: "))
+splitcalc = a/b
+print(f"Each should pay {splitcalc} $")
